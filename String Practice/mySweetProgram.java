5d485cdd6328317d452d9dbc2c96361acd175e63
Vaibhav Parmar <vgparmar@myseneca.ca>
2023-04-29 00:42:59 -0400

    Update mySweetProgram.java

--- String Practice/mySweetProgram.java
+++ String Practice/mySweetProgram.java
@@ -4,21 +4,37 @@ public class mySweetProgram {
 
 	public static void main(String[] args) {
 
-		//verify the password user enters, correct password is "dasnadas"
+		String name = "Vaibhav"; 
 		
-		String password = "dasnadas";
-		System.out.println("\nPlease enter the password: ");
+		System.out.println(String.format("Hello, %s", name));
 		
-		Scanner scanner = new Scanner(System.in);
-		String userEntry = scanner.nextLine();
+		System.out.println(name.length());
+		System.out.println(name.charAt(name.length()-1));
+		System.out.println(name.concat(" is my name!"));
 		
-		if(userEntry.equals(password)) {
-			System.out.println("\nLogged in successfully!\n");
-		}
-		else {
-			System.out.println("\nError! Wrong Password!\n");
-		}
+		System.out.println(name.contains("x"));
+		System.out.println(name.indexOf("V",2));
 		
+		System.out.println(name.toUpperCase());
+		System.out.println(name.toLowerCase());
+
+		String fullAd = " whatever happens, happens for the good   ";
+		System.out.println(fullAd);
+		
+		fullAd = " \t\nwhatever happens, happens for the good   ";
+		System.out.println(fullAd.strip());
+		//space as well as \t or \n also get removed in the strip command
+		
+		fullAd = "whatever happens, happens for the good";
+		System.out.println(fullAd.substring(9));
+		System.out.println(fullAd.substring(4, 16));
+		//in substring function the first index is inclusive and the second index is exclusive
+		
+		String das = "Das na Das!\n";
+		System.out.println(das.repeat(3));
+		System.out.println(das.equals("Das na Das"));
+		
+				
 	}
 
 }
