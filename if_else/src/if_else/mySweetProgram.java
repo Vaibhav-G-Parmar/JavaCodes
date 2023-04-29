d361a19353ce5a3358f4e98b1c4d68452ce2b401
Vaibhav Parmar <vgparmar@myseneca.ca>
2023-04-29 11:35:39 -0400

    Add files via upload

---
+++ if_else/src/if_else/mySweetProgram.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class mySweetProgram {
+
+	public static void main(String[] args) {
+				
+		String password = "dasnadas";
+		System.out.println("\nPlease enter the password: ");
+		
+		Scanner scanner = new Scanner(System.in);
+		String userEntry = scanner.nextLine();
+		
+		if(userEntry.equals(password)) {
+			System.out.println("\nLogged in successfully!\n");
+		}
+		else {
+			System.out.println("\nError! Wrong Password!\n");
+		}
+		
+
+	}
+	
+}
